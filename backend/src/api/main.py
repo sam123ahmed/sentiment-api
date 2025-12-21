6e532f8f523fd916543871e468b75495f6b31018
sam123ahmed <sameer016ahmed@gmail.com>
2025-12-22 01:46:29 +0530

    backend

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -15,3 +15,7 @@ def health():
 @app.post("/api/v1/predict")
 def predict(data: TextInput):
     return predict_sentiment(data.text)
+
+@app.post("/api/v1/predicting")
+def predicting():
+    return "tesing"
