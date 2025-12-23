a421d4411c6732d21a0241d5f21d103cfe93eda1
sam123ahmed <sameer016ahmed@gmail.com>
2025-12-24 00:48:02 +0530

    remove endpoint test

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from src.inference.predict import predict_sentiment
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Sentiment Analysis API", version="v1")
+
+# Allow requests from your frontend
+origins = [
+    "http://localhost:3000",   # for local development
+    "http://your-frontend-domain.com",  # for production
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # list of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],    # allow GET, POST, PUT, etc.
+    allow_headers=["*"],    # allow all headers
+)
+
 
 class TextInput(BaseModel):
     text: str
